9b60b87de9dec8a711d17afebb760347d0648aa0
epifanio <epiesasha@me.com>
2020-09-04 09:14:25 +0200

    todo: unit test

---
+++ test/tests_mmd_to_csw-iso.py
@@ -0,0 +1,6 @@
+# TODO: need to write unint test to:
+# - assert proper conversion is performed, it requires:
+#       - xslt translation file, 
+#       - mmd example file,
+#       - trusted and validated conversion of the mmd example file
+# - generarted output is a valid iso (requires iso xsd schema file)
