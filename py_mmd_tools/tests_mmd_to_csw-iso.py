fd14ba44b149a27de44f14eb9cd1182d06480259
epifanio <epiesasha@me.com>
2020-09-08 08:57:13 +0200

    adding test code for the mmd_to_iso method

---
+++ py_mmd_tools/tests_mmd_to_csw-iso.py
@@ -0,0 +1,61 @@
+import tempfile
+import unittest
+from mmd_to_csw_iso import mmd_to_iso
+import confuse
+
+
+class test_pymmdtools(unittest.TestCase):
+    def test_mmd2iso(self):
+        # Note:
+        # The "confuse" library is used to fetch the path string to the static files
+        # used to perform the test.
+        #
+        # On linux systems, by default, it reads a configuration file from:
+        # $HOME/.config/appname/config.yaml (in this case appname=mdtool)
+        #
+        # The confuse configurationfile uses yaml syntax, example:
+        #
+        # """
+        # paths:
+        #     reference_mmd: 'path/to/reference_mmd.xml'
+        #     reference_iso: 'path/to/reference_iso.xml'
+        #     mmd2isocsw: 'path/to/mmd-to-iso.xsl'
+        # """
+        #
+        # -- see the confuse documentation for more details. --
+        #
+        #
+        # If you do not want (or can not) use the 'confuse' library,
+        # replace the values of:
+        #
+        # reference_mmd
+        # reference_iso
+        # mmd2iso_xslt
+        #
+        # with the path strings to the files provided by your testing environment, and comment/remove the following lines:
+        #
+        # """
+        # import confuse
+        # config = confuse.Configuration('mmdtool', __name__)
+        # """
+        #
+        # run this test with:
+        # python3 -m unittest test_mmdtoiso.py
+        #
+        config = confuse.Configuration("mmdtool", __name__)
+        reference_mmd = config["paths"]["reference_mmd"].get()
+        reference_iso = config["paths"]["reference_iso"].get()
+        mmd2iso_xslt = config["paths"]["mmd2isocsw"].get()
+        self.maxDiff = None
+        tested = tempfile.mkstemp()[1]
+        mmd_to_iso(
+            mmd_file=reference_mmd,
+            outputfile=tested,
+            mmd2isocsw=mmd2iso_xslt,
+        )
+        with open(reference_iso) as reference, open(tested) as tested:
+            reference_iso_string = reference.read()
+            tested_string = tested.read()
+            unittest.TestCase.assertMultiLineEqual(
+                self, first=reference_iso_string, second=tested_string
+            )
