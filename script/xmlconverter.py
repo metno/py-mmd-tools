704d5bb1dccbc79656f8a08539256c613a2c52c4
epifanio <epiesasha@me.com>
2020-09-21 20:27:24 +0200

    removed config and logging to file to allow parallel execution
    adding option to recover unterminated conversion
    generalize code to allow xml to xml transformation
    using generic xslt

---
+++ script/xmlconverter.py
@@ -0,0 +1,139 @@
+import argparse
+import os
+import pathlib
+import sys
+import parmap
+from lxml.etree import XMLSyntaxError
+from xml2xml import xml2xml
+
+"""
+Script to run the xmlc onversion tools
+
+ License:
+     This file is part of the S-ENDA-Prototype repository (https://github.com/metno/py-mmd-tools).
+     S-ENDA-Prototype is licensed under GPL-3.0 (https://github.com/metno/py-mmd-tools/blob/master/LICENSE)
+
+usage: xmlconverter.py [-h] -i INPUT_DIR [-o OUTPUT_DIR] -t INPUT_XSLT
+                       [-r RECOVER_CONVERSION] [-p PARALLEL_CONVERSION]
+example: python3 xmlconverter.py -i metadata/nbs_mmdv2/ -t mmd2iso.xsl -p True -r True -o metadata/nbs_iso
+"""
+
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ("yes", "true", "t", "y", "1"):
+        return True
+    elif v.lower() in ("no", "false", "f", "n", "0"):
+        return False
+    else:
+        raise argparse.ArgumentTypeError("Boolean value expected.")
+    
+def filelist(directory):
+    print('os.walk("%s")' % directory)
+    xml_files = []
+    for subdir, dirs, files in os.walk(directory):
+        for file in files:
+            # print('File: %s' %file)
+            file_path = subdir + os.sep + file
+            if file_path.endswith(".xml"):
+                xml_files.append(file_path)
+    return xml_files
+
+def writerecord(xml_file, xslt, outdir="/tmp"):
+    pathlib.Path(outdir).mkdir(parents=True, exist_ok=True)
+    if not os.path.isfile(xslt):
+        raise Exception("XSLT file is missing: %s" % xslt)
+    outputfile = pathlib.PurePosixPath(outdir).joinpath(
+        pathlib.PurePosixPath(xml_file).name
+    )
+    xml2xml(
+        xml_file=xml_file,
+        outputfile=outputfile,
+        xslt=xslt,
+    )
+
+
+def main(metadata, xslt, outdir, recover=False, parallel=False):
+    if not recover:
+        xmlfiles = filelist(metadata)
+    else:
+        xmlfiles = recover_task(metadata, outdir, parallel)
+    print(f"Sprocessing {len(xmlfiles)} files")
+    if parallel is True:
+        print(f'parallel: {parallel}')
+        parmap.map(
+            writerecord,
+            xmlfiles,
+            xslt=xslt,
+            outdir=outdir,
+            pm_pbar=False,
+        )
+    else:
+        for i in xmlfiles:
+            try:
+                writerecord(xml_file=i, xslt=xslt, outdir=outdir)
+            except XMLSyntaxError:
+                print(f"failed on: {i}")
+
+
+def check_record(record, tobedone):
+    if pathlib.Path(record).stem in tobedone:
+        return record
+
+
+def recover_task(metadata, outdir, parallel=False):
+    total = filelist(metadata)
+    already_done = filelist(outdir)
+    total_stem = [pathlib.Path(i).stem for i in total]
+    already_done_stem = [pathlib.Path(i).stem for i in already_done]
+    tobedone_stem = list(set(total_stem) - set(already_done_stem))
+    if parallel:
+        print(f'parallel: {parallel}')
+        tobedone = parmap.map(check_record, total, tobedone_stem, pm_pbar=False)
+    else:
+        tobedone = [check_record(i, tobedone_stem) for i in total]
+    tobedone_files = [i for i in tobedone if i is not None]
+    return tobedone_files
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Convert xml files using XSL Tranformation")
+    parser.add_argument(
+        "-i", "--input-dir", help="directory with input XML", required=True
+    )
+    parser.add_argument("-o", "--output-dir", help="outpout directory with ISO")
+    parser.add_argument(
+        "-t", "--input-xslt", help="input xslt translation file", required=True
+    )
+    parser.add_argument(
+        "-r",
+        "--recover-conversion",
+        type=str2bool,
+        const=False,
+        help="recover a previously interrupted conversion",
+        default=False,
+        required=False,
+    )
+    parser.add_argument(
+        "-p",
+        "--parallel-conversion",
+        type=str2bool,
+        const=False,
+        help="recover a previously interrupted conversion",
+        default=False,
+        required=False,
+    )
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    main(
+        metadata=args.input_dir,
+        xslt=args.input_xslt,
+        outdir=args.output_dir,
+        recover=args.recover_conversion,
+        parallel=args.parallel_conversion,
+    )
