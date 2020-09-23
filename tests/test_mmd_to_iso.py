ad1eeab372332d1b786ae6d32aa1d9d56f48478f
Elodie Fernandez <elodief@met.no>
2020-09-23 08:33:54 +0200

    Fix for tests.

--- tests/test_mmd_to_iso.py
+++ tests/test_mmd_to_iso.py
@@ -6,28 +6,25 @@ from py_mmd_tools.mmd_to_csw_iso import mmd_to_iso
 
 class test_pymmdtools(unittest.TestCase):
     def setUp(self):
-        # run this test from the tests directory by running:
         #
-        # python3 test_mmd_to_iso.py
-        #
-        # or from the root directory by running:
+        # run this test from the root directory by running:
         #
         # python3 -m unittest 
         #
         # unset the output limit when printing the xml diff
         #
         current_dir = pathlib.Path.cwd()
-        self.reference_mmd = current_dir / 'tests' / 'data' / 'reference_mmd.xml'
-        self.reference_iso = current_dir / 'tests' / 'data' / 'reference_iso.xml'
-        self.mmd2iso_xslt = current_dir / 'tests' / 'data' / 'mmd-to-iso.xsl'
+        self.reference_mmd = str(current_dir / 'tests' / 'data' / 'reference_mmd.xml')
+        self.reference_iso = str(current_dir / 'tests' / 'data' / 'reference_iso.xml')
+        self.mmd2iso_xslt = str(current_dir / 'tests' / 'data' / 'mmd-to-iso.xsl')
 
     def test_mmd2iso(self):
         self.maxDiff = None
         tested = tempfile.mkstemp()[1]
         mmd_to_iso(
-            mmd_file=str(self.reference_mmd),
+            mmd_file=self.reference_mmd,
             outputfile=tested,
-            mmd2isocsw=str(self.mmd2iso_xslt),
+            mmd2isocsw=self.mmd2iso_xslt,
         )
         with open(self.reference_iso) as reference, open(tested) as tested:
             reference_iso_string = reference.read()
