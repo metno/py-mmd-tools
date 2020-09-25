0bdf1c21a2eb5776d340c4fae370f12a7349c0f6
epifanio <epiesasha@me.com>
2020-09-25 17:25:22 +0200

    adding test for logging function

---
+++ tests/test_log_util.py
@@ -0,0 +1,40 @@
+import tempfile
+import unittest
+import pathlib
+import confuse
+import logging
+from py_mmd_tools.log_util import get_logpath, setup_log
+
+class test_pymmdtools(unittest.TestCase):
+    def setUp(self):
+        #
+        # run this test from the root directory by running:
+        #
+        # python3 -m unittest 
+        #
+        # unset the output limit when printing the xml diff
+        #
+        current_dir = pathlib.Path.cwd()
+        # self.not_a_dir = current_dir / 'tests' / 'data' / 'not_a_dir'
+
+    def test_get_logpath_1(self):
+        logpath = get_logpath()
+        unittest.TestCase.assertEqual(self, pathlib.Path(logpath).is_dir(), 
+                                    True)
+
+    def test_get_logpath_2(self):
+        logpath = get_logpath(config_name='config_test')
+        unittest.TestCase.assertEqual(self, pathlib.Path(logpath).is_dir(), 
+                                    True)
+
+    def test_setup_log_1(self):
+        unittest.TestCase.assertEqual(self, type(setup_log('test')), 
+                                    type(logging.getLogger('test')))
+
+    def test_setup_log_2(self):
+        unittest.TestCase.assertEqual(self, type(setup_log('test', logtype='stream')), 
+                                    type(logging.getLogger('test')))
+
+    def test_setup_log_3(self):
+        unittest.TestCase.assertEqual(self, type(setup_log('test', logtype='file')), 
+                                    type(logging.getLogger('test')))
