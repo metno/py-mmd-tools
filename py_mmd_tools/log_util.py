3983d570ff02e62389b8859b86988465bc3e84e6
epifanio <epiesasha@me.com>
2020-09-26 15:13:03 +0200

    fixing test

--- py_mmd_tools/log_util.py
+++ py_mmd_tools/log_util.py
@@ -1,28 +1,30 @@
-import confuse
-from confuse.exceptions import NotFoundError
 import logging
 import pathlib
+import os
 
-def get_logpath(config_name='mmdtool'):
-    try:
-        config = confuse.Configuration(config_name, __name__)
-        logfilepath = config["paths"]["logs"].get()
-    except NotFoundError:
-        logfilepath = "./logs/"
-    if not pathlib.Path(logfilepath).exists():
-        pathlib.Path(logfilepath).mkdir(parents=True, exist_ok=True)
-    return logfilepath
+def get_logpath(logdirpath):
+    if not pathlib.Path(logdirpath).exists():
+        try:
+            pathlib.Path(logdirpath).mkdir(parents=True, exist_ok=True)
+            return logdirpath
+        except IOError:
+            raise
+    else:
+        if os.access(logdirpath, os.W_OK):
+            return logdirpath
+        else:
+            raise IOError
 
 
-def setup_log(name, logtype='stream'):
+def setup_log(name, logdirpath, logtype='stream'):
     ''' logtype = [file, stream]'''
     logger = logging.getLogger(name)  # > set up a new name for a new logger
     logger.setLevel(logging.DEBUG)  # here is the missing line
     log_format = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
     if logtype=='file':
         # filehandler
-        logfilepath = get_logpath()
-        filename = pathlib.Path(logfilepath, f"{name}.log")
+        logdirpath = get_logpath(logdirpath)
+        filename = pathlib.Path(logdirpath, f"{name}.log")
         fh = logging.FileHandler(filename, mode='w', encoding='utf-8')
         fh.setLevel(logging.DEBUG)
         fh.setFormatter(log_format)
--- tests/test_log_util.py
+++ tests/test_log_util.py
@@ -1,9 +1,9 @@
 import tempfile
 import unittest
 import pathlib
-import confuse
 import logging
 from py_mmd_tools.log_util import get_logpath, setup_log
+import uuid 
 
 class test_pymmdtools(unittest.TestCase):
     def setUp(self):
@@ -14,27 +14,26 @@ class test_pymmdtools(unittest.TestCase):
         #
         # unset the output limit when printing the xml diff
         #
-        current_dir = pathlib.Path.cwd()
+        self.current_dir = pathlib.Path.cwd()
+        self.not_a_writable_dir =  pathlib.Path('/') / pathlib.Path(uuid.uuid4().hex[:6].upper())
         # self.not_a_dir = pathlib.Path(current_dir) / 'tests' / 'data' / 'not_a_dir'
 
     def test_get_logpath_1(self):
-        logpath = get_logpath()
+        logpath = get_logpath(self.current_dir)
         unittest.TestCase.assertEqual(self, pathlib.Path(logpath).is_dir(), 
                                     True)
 
     def test_get_logpath_2(self):
-        logpath = get_logpath(config_name='config_test')
-        unittest.TestCase.assertEqual(self, pathlib.Path(logpath).is_dir(), 
-                                    True)
+        self.assertRaises(IOError, get_logpath, self.not_a_writable_dir)
 
     def test_setup_log_1(self):
-        unittest.TestCase.assertEqual(self, type(setup_log('test')), 
+        unittest.TestCase.assertEqual(self, type(setup_log('test',  logdirpath=self.current_dir)), 
                                     type(logging.getLogger('test')))
 
     def test_setup_log_2(self):
-        unittest.TestCase.assertEqual(self, type(setup_log('test', logtype='stream')), 
+        unittest.TestCase.assertEqual(self, type(setup_log('test', logdirpath=self.current_dir, logtype='stream')), 
                                     type(logging.getLogger('test')))
 
     def test_setup_log_3(self):
-        unittest.TestCase.assertEqual(self, type(setup_log('test', logtype='file')), 
+        unittest.TestCase.assertEqual(self, type(setup_log('test', logdirpath=self.current_dir, logtype='file')), 
                                     type(logging.getLogger('test')))
