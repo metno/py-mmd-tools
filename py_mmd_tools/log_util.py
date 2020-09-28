d507836a21c59041076a56b7c2dd6fc7cea02b97
epifanio <epiesasha@me.com>
2020-09-28 09:59:22 +0200

    Xml2xml (#14)
    * new utility code
    removed  config and logger to allow parallel usage
    
    * generalized code to convert between xml scheme
    removed config and logging to file to allow paralle execution
    
    * removed config and logging to file
    to allow parallel execution
    
    adding option to recover unterminated conversion
    generalize code to allow xml to xml transformation
    using generic xslt
    
    * adding test (untested) for xml2xml method.
    
    * adding a logger with option
    to log to file or stream
    
    * fixing parsearg options
    
    * adding log_util to separate logging
     from the rest of the code
    
    * fixing import
    
    * removed logging utility
    fixing exceptions
    
    * adding test
    
    * adding test data
    
    * fixing import
    
    * adding test for logging function
    
    * fix pathlib string
    
    * helper config to show lines missing coverage
    
    * removing no longer used confuse lib
    
    * fixing test
    
    * clean up for test
    
    * adding reference to license
    
    * code refactoring
    replace mmd specific names with
    more generic xml
    as the tool in the available code
     execute generic transformation on xml files
    
    * adding dependency
    
    * coomenting a test which
    works locally but fails on codecov

---
+++ py_mmd_tools/log_util.py
@@ -0,0 +1,46 @@
+"""
+Tool for converting metadata from MMD format to ISO format using a specific xslt.
+ License:
+     This file is part of the S-ENDA-Prototype repository (https://github.com/metno/py-mmd-tools).
+     S-ENDA-Prototype is licensed under GPL-3.0 (https://github.com/metno/py-mmd-tools/blob/master/LICENSE)
+"""
+
+import logging
+import pathlib
+import os
+
+def get_logpath(logdirpath):
+    if not pathlib.Path(logdirpath).exists():
+        try:
+            pathlib.Path(logdirpath).mkdir(parents=True, exist_ok=True)
+            return logdirpath
+        except IOError:
+            raise
+    else:
+        if os.access(logdirpath, os.W_OK):
+            return logdirpath
+        else:
+            raise IOError
+
+
+def setup_log(name, logdirpath, logtype='stream'):
+    ''' logtype = [file, stream]'''
+    logger = logging.getLogger(name)  # > set up a new name for a new logger
+    logger.setLevel(logging.DEBUG)  # here is the missing line
+    log_format = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    if logtype=='file':
+        # filehandler
+        logdirpath = get_logpath(logdirpath)
+        filename = pathlib.Path(logdirpath, f"{name}.log")
+        fh = logging.FileHandler(filename, mode='w', encoding='utf-8')
+        fh.setLevel(logging.DEBUG)
+        fh.setFormatter(log_format)
+        logger.addHandler(fh)
+        return logger
+    if logtype=='stream':
+        # streamhandler
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+        ch.setFormatter(log_format)
+        logger.addHandler(ch)
+        return logger
