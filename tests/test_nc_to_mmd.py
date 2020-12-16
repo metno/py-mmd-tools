db47dd56766c059710ffc91c3242afb9651f6a9a
Elodie Fernandez <elodief@met.no>
2020-12-16 16:10:42 +0100

    Cleaning

--- tests/test_nc_to_mmd.py
+++ tests/test_nc_to_mmd.py
@@ -3,7 +3,7 @@ import unittest
 import pathlib
 import tempfile
 from py_mmd_tools.nc_to_mmd import Nc_to_mmd
-#from mmd_utils.nc_to_mmd import main as main_nc_to_mmd
+
 
 class TestNC2MMD(unittest.TestCase):
 
