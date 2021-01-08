36072c6bbe25ddb10c9315670672258c2abecdd8
Morten W. Hansen <morten.wergeland.hansen@gmail.com>
2021-01-08 11:52:12 +0100

    #20: docker container updated with required software, yaml file and test updated

--- tests/test_mmd_yaml_vs_xsd.py
+++ tests/test_mmd_yaml_vs_xsd.py
@@ -6,32 +6,6 @@ import unittest
 
 import py_mmd_tools
 
-#class ParametrizedTestCase(unittest.TestCase):
-#    """ TestCase classes that want to be parametrized should
-#        inherit from this class.
-#    """
-#    def __init__(self, methodName='runTest', xsd_eattr=None, yaml_eattr=None):
-#        super(ParametrizedTestCase, self).__init__(methodName)
-#        self.xsd_eattr = xsd_eattr
-#        self.yaml_eattr = yaml_eattr
-#
-#    @staticmethod
-#    def parametrize(testcase_class, xsd_eattr=None, yaml_eattr=None):
-#        """ Create a suite containing all tests taken from the given
-#            subclass, passing them the parameter 'param'.
-#        """
-#        testloader = unittest.TestLoader()
-#        testnames = testloader.getTestCaseNames(testcase_class)
-#        suite = unittest.TestSuite()
-#        for name in testnames:
-#            suite.addTest(testcase_class(name, xsd_eattr=xsd_eattr, yaml_eattr=yaml_eattr))
-#        return suite
-#
-#class TestMMDYamlVsXSD(ParametrizedTestCase):
-#
-#    def test_element_attribute(self):
-#        self.assertEqual(self.xsd_eattr, self.yaml_eattr)
-
 class TestMDDElementsInYAMLAndXSD(unittest.TestCase):
 
     def test_mmd_element(self):
@@ -50,7 +24,11 @@ class TestMDDElementsInYAMLAndXSD(unittest.TestCase):
             self.assertEqual(elem['@maxOccurs'], mmd_yaml[elem['@name']]['maxOccurs'], msg=elem['@name'])
             self.assertEqual(elem['@minOccurs'], mmd_yaml[elem['@name']]['minOccurs'], msg=elem['@name'])
 
-#suite = unittest.TestSuite()
-#suite.addTest(ParametrizedTestCase.parametrize(TestMMDYamlVsXSD, xsd_eattr=42, yaml_eattr=42))
-#suite.addTest(ParametrizedTestCase.parametrize(TestMMDYamlVsXSD, xsd_eattr=13, yaml_eattr=42))
-#unittest.TextTestRunner(verbosity=2).run(suite)
+        # Loop elements under xs:choice
+        for elem in ll['xs:sequence']['xs:choice']['xs:element']:
+            self.assertIn(elem['@name'], mmd_yaml.keys())
+            self.assertEqual(elem['@maxOccurs'], mmd_yaml[elem['@name']]['maxOccurs'], msg=elem['@name'])
+            self.assertEqual(elem['@minOccurs'], mmd_yaml[elem['@name']]['minOccurs'], msg=elem['@name'])
+
+if __name__ == '__main__':
+    unittest.main()
