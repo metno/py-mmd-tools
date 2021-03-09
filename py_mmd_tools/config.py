729bc7a02bd04ee59b28e1e9854492687f190247
Trygve Aspenes <Trygve.Aspenes@met.no>
2021-03-09 14:04:51 +0100

    Update py_mmd_tools/config.py
    Co-authored-by: Morten W. Hansen <109061+mortenwh@users.noreply.github.com>

--- py_mmd_tools/config.py
+++ py_mmd_tools/config.py
@@ -5,14 +5,6 @@ PY-MMD-TOOLS : Package Config Class
 License:
      This file is part of the py-mmd-tools repository (https://github.com/metno/py-mmd-tools). py-mmd-tools is licensed under the Apache License 2.0 (https://github.com/metno/py-mmd-tools/blob/master/LICENSE)
 
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
 """
 
 import sys
