09d079e11636ce267cc592b04d20b1aeaa99634c
Trygve Aspenes <Trygve.Aspenes@met.no>
2021-03-09 21:54:45 +0100

    #70: Need config.py file to allow for installation (#71)
    * Kind of working version.
    
    * Update py_mmd_tools/config.py
    
    Co-authored-by: Morten W. Hansen <109061+mortenwh@users.noreply.github.com>
    
    * Update py_mmd_tools/config.py
    
    Co-authored-by: Morten W. Hansen <109061+mortenwh@users.noreply.github.com>
    
    * Update py_mmd_tools/config.py
    
    Co-authored-by: Morten W. Hansen <109061+mortenwh@users.noreply.github.com>
    
    * Update py_mmd_tools/config.py
    
    Co-authored-by: Morten W. Hansen <109061+mortenwh@users.noreply.github.com>
    
    * Update py_mmd_tools/config.py
    
    Co-authored-by: Morten W. Hansen <109061+mortenwh@users.noreply.github.com>
    
    * Update py_mmd_tools/config.py
    
    Co-authored-by: Morten W. Hansen <109061+mortenwh@users.noreply.github.com>
    
    * Update setup.cfg
    
    Co-authored-by: Morten W. Hansen <109061+mortenwh@users.noreply.github.com>
    
    * Update setup.cfg
    
    Co-authored-by: Morten W. Hansen <109061+mortenwh@users.noreply.github.com>
    
    * Update setup.cfg
    
    Co-authored-by: Morten W. Hansen <109061+mortenwh@users.noreply.github.com>
    
    * Update setup.py
    
    Co-authored-by: Morten W. Hansen <109061+mortenwh@users.noreply.github.com>
    
    Co-authored-by: Trygve Aspenes <trygveas@met.no>
    Co-authored-by: Morten W. Hansen <109061+mortenwh@users.noreply.github.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+PY-MMS : Setup Script
+License:
+     This file is part of the py-mmd-tools repository (https://github.com/metno/py-mmd-tools). py-mmd-tools is licensed under the Apache License 2.0 (https://github.com/metno/py-mmd-tools/blob/master/LICENSE).
+"""
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
