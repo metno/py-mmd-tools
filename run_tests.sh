c026953102ab1614da77200f3484da45ea3a0a7b
Morten W. Hansen <morten.wergeland.hansen@gmail.com>
2021-04-19 13:58:36 +0200

    make sure we don't use the codecov bash uploader

--- run_tests.sh
+++ run_tests.sh
@@ -6,16 +6,10 @@ nosetests --with-coverage --cover-xml --cover-xml-file=/src/coverage.xml --cover
 
 echo
 
-if [[ -n "$CODECOV_TOKEN" ]]
-then
-  bash <(curl -s https://codecov.io/bash)
-else
-  echo %%
-  echo %% Do you want code coverage generated on https://codecov.io with GitHub Actions.
-  echo %%
-  echo "%%   1. Add repository to codecov.io."
-  echo "%%   2. Add a repository secret in [ Setting > Secrets ]."
-  echo "%%      Name it CODECOV_TOKEN, and use the token from codecov.io as value."
-  echo %%
-fi
-
+echo %%
+echo %% Do you want code coverage generated on https://codecov.io with GitHub Actions.
+echo %%
+echo "%%   1. Add repository to codecov.io."
+echo "%%   2. Add a repository secret in [ Setting > Secrets ]."
+echo "%%      Name it CODECOV_TOKEN, and use the token from codecov.io as value."
+echo %%
