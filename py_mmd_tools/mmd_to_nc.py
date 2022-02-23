b8c8ef95687f1c2c2e6b6a6963520ffe9bfa391a
Elodie Fernandez <elodief@met.no>
2022-02-23 14:09:28 +0100

    flake

--- py_mmd_tools/mmd_to_nc.py
+++ py_mmd_tools/mmd_to_nc.py
@@ -168,8 +168,8 @@ class Mmd_to_nc(object):
 
     def process_personnel(self, element):
         """
-        Special case for MMD element "personnel". Since its child elements are related one to another,
-        they must to be processed simultaneously.
+        Special case for MMD element "personnel". Since its child elements are related one to
+        another, they must to be processed simultaneously.
 
         Input
         ====
