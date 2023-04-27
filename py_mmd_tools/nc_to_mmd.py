f9fb6436e4836f32cadefc3f8cde09ef0f0e4209
Morten W. Hansen <morten.wergeland.hansen@gmail.com>
2023-04-27 11:52:44 +0200

    #253: fix flake error

--- py_mmd_tools/nc_to_mmd.py
+++ py_mmd_tools/nc_to_mmd.py
@@ -1389,7 +1389,7 @@ class Nc_to_mmd(object):
         # Check that the OPeNDAP url is accessible
         try:
             ds = Dataset(self.opendap_url)
-        except OSError as oe:
+        except OSError:
             msg = "Cannot access OPeNDAP stream: %s" % self.opendap_url
             self.missing_attributes['warnings'].append(msg)
         else:
