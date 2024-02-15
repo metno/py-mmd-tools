a848e2d6dc74d021171afb1ae7ae6fd3cd3b1625
Morten W. Hansen <109061+mortenwh@users.noreply.github.com>
2024-02-15 10:13:41 +0100

    Add ncheader2json script (#311)
    * Add ncheader2mmd script
    
    * #262: initial test code
    
    * Add option to send json output to stdout
    
    * Add missing braket
    
    * Update formatting to flake8
    
    * Update formatting to flake8
    
    * Removed unused imports
    
    * Fixed call of get_header_netCDF and comparison of json
    
    * Fixed loading of json example
    
    * Fixed path for reference header file
    
    * Load dict to json object first
    
    * Fixed index access for single header
    
    * Changed logic for writing file
    
    * Removed unnecessary test
    
    * add negative test
    
    * Add load dict to json object
    
    * Add read folder call
    
    * Add read folder call
    
    * Add path serach in sub folder
    
    * Chnage pattern match
    
    * Revert to old pattern
    
    * Add tempdir creation to test read folder
    
    * Add generated json to output
    
    * Add actual running of main functino
    
    * Fixed writing of wrong ibject
    
    * Removed unused imports
    
    * Removed folder read functionality
    
    * Remove use of list
    
    * Acctually removed use of list
    
    * Update py_mmd_tools/script/ncheader2json.py
    
    Co-authored-by: Morten W. Hansen <109061+mortenwh@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Morten W. Hansen <109061+mortenwh@users.noreply.github.com>
    
    * Update py_mmd_tools/nc_to_mmd.py
    
    Co-authored-by: Morten W. Hansen <109061+mortenwh@users.noreply.github.com>
    
    * Add docstring
    
    * Conform to flake8
    
    * Removed unused variable
    
    * Reverted automatic formatting
    
    * Fixed type
    
    * Add more docstring
    
    * Add example of usage in API
    
    * Update py_mmd_tools/script/ncheader2json.py
    
    * Add line too long
    
    * Split line too long
    
    ---------
    
    Co-authored-by: Amund Isaksen <amundi@met.no>
    Co-authored-by: Amund Isaksen <31344542+Teddy-1000@users.noreply.github.com>

---
+++ py_mmd_tools/script/ncheader2json.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+
+"""
+Tool for extracting netCDF header to a json. For use with the py-mmd-tools API.
+
+
+Usage:
+
+ncheader2json -i <path-to-nc-file> |
+curl -X POST -H 'Content-Type: application/json'  -d @- "<url-to-api>"
+
+License:
+
+This file is part of the py-mmd-tools repository
+<https://github.com/metno/py-mmd-tools>.
+
+py-mmd-tools is licensed under the Apache License 2.0
+<https://github.com/metno/py-mmd-tools/blob/master/LICENSE>
+"""
+import json
+import argparse
+import pathlib
+
+import numpy as np
+
+
+from netCDF4 import Dataset
+
+
+def create_parser():
+    """Create parser object"""
+
+    parser = argparse.ArgumentParser(
+        description="Extract nc header to json format. Usage in api: ncheader2json"
+        " -i <path-to-nc-file> |"
+        " curl -X POST -H 'Content-Type: application/json'  -d @- '<url-to-api>'"
+    )
+
+    parser.add_argument("-i", "--input", type=str, help="Input file", required=True)
+
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        help="Output json file, if unset the json is dumped to stdout.",
+        required=False,
+    )
+
+    parser.add_argument(
+        "-e", "--file-ending", default="nc", type=str, help="File ending of nc files"
+    )
+
+    return parser
+
+
+def get_header_netCDF(data: Dataset) -> dict:
+    """
+    This function grabs all global and variable attributes and dumps them into a json file.
+    """
+    full_attr = {
+        "global_variables": {i: data.getncattr(i) for i in data.ncattrs()},
+        "variables": {},
+    }
+
+    for var_name, variable in data.variables.items():
+        full_attr["variables"][var_name] = {
+            "attrs": {i: handle_numpy_types(variable.getncattr(i)) for i in variable.ncattrs()},
+            "dtype": str(variable.dtype),
+            "shape": variable.shape,
+        }
+
+    return json.dumps(full_attr)
+
+
+def handle_numpy_types(inpt):
+    """
+    Some numpy types can not be converted or parsed to a json object.
+    This functions handles conversion from un-parsable types, to parsable ones.
+    """
+    if isinstance(inpt, np.float32):
+        return np.float64(inpt)
+
+    if isinstance(inpt, np.int64):
+        return inpt.item()
+
+    if isinstance(inpt, np.ndarray):
+        return inpt.tolist()
+
+    return inpt
+
+
+def main(args=None):
+    """Main function for this script"""
+    if pathlib.Path(args.input).is_file():
+        inputfiles = [args.input]
+    else:
+        raise ValueError(f"Invalid input: {args.input}")
+
+    json_header = [json.loads(get_header_netCDF(Dataset(file))) for file in inputfiles][0]
+
+    if args.output:
+        with open(args.output, "w") as fp:
+            json.dump(json_header, fp)
+    else:
+        print(json_header)
+
+    return json_header
+
+
+def _main():  # pragma: no cover
+    main(create_parser().parse_args())  # entry point
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main(create_parser().parse_args())
---
+++ tests/test_ncheader2json.py
@@ -0,0 +1,79 @@
+"""
+License:
+
+This file is part of the py-mmd-tools repository
+<https://github.com/metno/py-mmd-tools>.
+
+py-mmd-tools is licensed under the Apache License 2.0
+<https://github.com/metno/py-mmd-tools/blob/master/LICENSE>
+"""
+
+import os
+import json
+import pytest
+import tempfile
+
+import numpy as np
+
+from py_mmd_tools.script.ncheader2json import create_parser
+from py_mmd_tools.script.ncheader2json import main
+from py_mmd_tools.script.ncheader2json import get_header_netCDF
+from py_mmd_tools.script.ncheader2json import handle_numpy_types
+from netCDF4 import Dataset
+
+
+@pytest.mark.script
+def test_main(dataDir):
+    """Test that the created json file contains correct dataset id"""
+    parser = create_parser()
+    test_in = os.path.join(dataDir, "reference_nc.nc")
+    fd, test_out = tempfile.mkstemp(suffix=".json")
+    parsed = parser.parse_args(["-i", test_in, "-o", test_out])
+    main(parsed)
+    with open(test_out, "r") as fp:
+        ncheader = json.load(fp)
+    assert ncheader["global_variables"]["id"] == "b7cb7934-77ca-4439-812e-f560df3fe7eb"
+    os.remove(test_out)
+    parsed = parser.parse_args(["-i", test_in])
+    ncheader = main(parsed)
+    assert ncheader["global_variables"]["id"] == "b7cb7934-77ca-4439-812e-f560df3fe7eb"
+
+
+@pytest.mark.script
+def test_main_raise_error():
+    """
+    Test that ncheader2json rasies expected exception.
+    """
+    parser = create_parser()
+    parsed = parser.parse_args(["-i", "this_path_should_not_exsist"])
+    with pytest.raises(ValueError):
+        main(args=parsed)
+
+
+@pytest.mark.script
+@pytest.mark.parametrize(
+    "dataDir, test_input, expected",
+    [
+        ("_", np.float32(0.1), np.float64),
+        ("_", np.int64(1), int),
+        ("_", np.ndarray([1, 2, 3]), list),
+    ],
+)
+def test_handle_numpy_types(dataDir, test_input, expected):
+    """
+    Assert that numpy types are handled as expected
+    """
+    assert isinstance(handle_numpy_types(test_input), expected)
+
+
+@pytest.mark.script
+def test_get_header_netCDF(dataDir):
+    """
+    Test that the script returns expected netCDF header.
+    """
+    with open(os.path.join(dataDir, "reference_nc_header.json")) as file:
+        expected = json.load(file)
+    test_input = get_header_netCDF(Dataset(os.path.join(dataDir, "reference_nc.nc")))
+    assert json.dumps(json.loads(test_input), sort_keys=True) == json.dumps(
+        expected, sort_keys=True
+    )
