74579a5aefa01b9c4d070a875d1954d5008d60ed
Amund Isaksen <31344542+Teddy-1000@users.noreply.github.com>
2024-06-24 14:02:54 +0200

    add handeling of np int32 (#334)
    * add handeling of np int32
    
    * add test

--- py_mmd_tools/script/ncheader2json.py
+++ py_mmd_tools/script/ncheader2json.py
@@ -92,6 +92,9 @@ def handle_numpy_types(inpt):
     if isinstance(inpt, np.ndarray):
         return inpt.tolist()
 
+    if isinstance(inpt, np.int32):
+        return inpt.item()
+
     return inpt
 
 
